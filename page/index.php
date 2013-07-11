a7417399e7ff8a76a223095d1a0fa5cb008f770d
Gowrav Vishwakarma <gowrav@xserver.(none)>
2013-07-11 18:04:13 +0530

    base ready

--- page/index.php
+++ page/index.php
@@ -3,16 +3,9 @@ class page_index extends Page {
 	function init(){
 		parent::init();
 
-		$this->add('documenting/View_Example')
-            ->set('
-            		$form=$this->add("Form",null,"Demo");
-            		$form->addField("line","var1");
-
-            		if($form->isSubmitted()){
-            			$form->displayError("var1","teri to ...");
-            		}
-
-            	');
-
 	}
+
+      function defaultTemplate(){
+            return array('page/toc');
+      }
 }
