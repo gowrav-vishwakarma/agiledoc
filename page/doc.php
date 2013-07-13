04e3374eb3e97a63f0202da6c339c87f24788f1a
Gowrav Vishwakarma <gowrav@xserver.(none)>
2013-07-13 12:45:05 +0530

    images lightbox added, video to correct

--- page/doc.php
+++ page/doc.php
@@ -16,13 +16,22 @@ class page_doc extends Page {
     function init(){
     	parent::init();
 		$page=$this;
-		
-    	$page->template->eachTag('Code',function($a,$b) use($page){
+
+        
+        $page->template->eachTag('Code',function($a,$b) use($page){
             $page->add('documenting/View_Example',null,$b)->set($a,true);
         });
 
         $page->template->eachTag('Example',function($a,$b) use($page){
             $page->add('documenting/View_Example',null,$b)->set($a);
+        });
+        
+        $page->template->eachTag('image_preview',function($a,$b) use($page){
+            $page->add('Text',null,$b)->setHTML("<a class='fancybox' href='templates/default/$a'><img width='20px' height='20px' src='templates/default/images/screenshot_preview.jpeg'/></a>");
+        });
+
+        $page->template->eachTag('youtube',function($a,$b) use($page){
+            $page->add('Text',null,$b)->setHTML("<a class='fancybox-media' href='$a'><img width='20px' height='20px' src='templates/default/images/video_icon.jpeg'/></a>");
         });
 
     }
